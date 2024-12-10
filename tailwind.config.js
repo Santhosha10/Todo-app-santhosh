6823798100c201b46d2182b77a5cf992f97d8bec
Santhosh20030610 <santhosh20030610@gmail.com>
2024-12-10 16:50:11 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage:{
-        'login-bg':"url('my_react\src\bg.jpg.webp')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
